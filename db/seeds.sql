b9189e18a92a31e06e11a23751900e882b01b35d
JMHupfer <jmhupfer@gmail.com>
2023-05-14 15:48:24 -0600

    added updates to schema.sql and seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,9 +3,9 @@ VALUES (1, 'Sacred Heart Hospital');
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (1, 'Surgeon', 120000.00, 1),
-       (2, 'Medical Doctor', 110000.00, 1)
+       (2, 'Medical Doctor', 110000.00, 1),
        (3, 'Nurse', 70000.00, 1),
-       (4, 'Lawyer', 45000.00, 1)
+       (4, 'Lawyer', 45000.00, 1),
        (5, 'Janitor', 30000.00, 1);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
@@ -16,4 +16,4 @@ VALUES (1, 'Christopher', 'Turk', 1, NULL),
        (5, 'Carla', 'Espinoza', 3, 1),
        (6, 'Laverne', 'Robers', 3, NULL),
        (7, 'Tedd', 'Buckland', 4, NULL),
-       (8, 'Janitor', '', 5, NULL)
+       (8, 'Janitor', '', 5, NULL);
