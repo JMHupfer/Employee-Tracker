708fa11011c5739e104ce76cfd8c224daeae31c4
JMHupfer <jmhupfer@gmail.com>
2023-05-14 15:29:32 -0600

    adding code to schema.sql and seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department {
+    id INT PRIMARY KEY,
+    dep_name VARCHAR(30)
+};
+
+CREATE TABLE role {
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+};
+
+CREATE TABLE employee {
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+};
---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department(id, dep_name)
+VALUES (1, 'Sacred Heart Hospital');
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, 'Surgeon', 120000.00, 1),
+       (2, 'Medical Doctor', 110000.00, 1)
+       (3, 'Nurse', 70000.00, 1),
+       (4, 'Lawyer', 45000.00, 1)
+       (5, 'Janitor', 30000.00, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, 'Christopher', 'Turk', 1, NULL),
+       (2, 'The', 'Todd', 1, NULL),
+       (3, 'John', 'Dorian', 2, NULL),
+       (4, 'Perry', 'Cox', 2, 1),
+       (5, 'Carla', 'Espinoza', 3, 1),
+       (6, 'Laverne', 'Robers', 3, NULL),
+       (7, 'Tedd', 'Buckland', 4, NULL),
+       (8, 'Janitor', '', 5, NULL)
